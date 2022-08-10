5a0ae6f1a04cb339aba2795d64429cd2ebdf3c85
doruk eray <doruk@dorkodu.com>
2022-08-10 23:58:52 +0300

    rearrange universal type definitions into monoliths

--- packages/server/source/type/index.ts
+++ packages/server/source/type/index.ts
@@ -1,14 +1,10 @@
-import { Maybe } from "../types";
+import { Maybe } from "../utils";
 
 export interface SageDefinition {
   readonly name: string;
 }
 
-type SageArtifactFunction = (reference: Maybe<object>) => any;
-
-export interface SageDefinition {
-  readonly name: string;
-}
+type SageArtifactFunction = (context: Maybe<object>) => any;
 
 export interface SageAttribute extends SageDefinition {
   readonly rule?: (value: any) => boolean;
@@ -28,8 +24,17 @@ export interface SageResource extends SageDefinition {
   readonly attributes?: { [key: string]: SageAttribute };
   readonly acts?: { [key: string]: SageAct };
   readonly links?: { [key: string]: SageLink };
-  readonly resolve: (query: SageQuery, context: Maybe<object>) => Maybe<object>;
+  readonly resolve: (
+    query?: SageQuery,
+    context?: Maybe<object>
+  ) => Maybe<object>;
 }
+
+export interface SageSchema {
+  readonly types: { [key: string]: SageResource };
+}
+
+//? Document
 
 export interface SageQuery {
   readonly name: string;
@@ -40,6 +45,13 @@ export interface SageQuery {
   readonly links?: { [key: string]: SageQuery };
 }
 
-export interface SageSchema {
-  readonly types: { [key: string]: SageResource };
+export interface SageDocument {
+  [key: string]: SageQuery;
+}
+
+//? execution
+export interface SageExecutionResult {
+  data: object;
+  error: object;
+  meta: object;
 }
--- packages/server/source/type/schema.ts
+++
@@ -1,9 +0,0 @@
-import { ISageSchema, ISageResource } from "./interfaces";
-
-export class SageSchema implements ISageSchema {
-  public types: { [key: string]: ISageResource };
-
-  constructor(types: { [key: string]: ISageResource }) {
-    this.types = types;
-  }
-}
