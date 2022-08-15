8a257c07d274c4cbe98b96b64e15b63eba632f20
doruk eray <doruk@dorkodu.com>
2022-08-15 23:41:37 +0300

    update sage's index
    - switch to sage\problem for error handling.
    - implement validateSchema()
    - structure parse()

--- packages/server/source/index.ts
+++ packages/server/source/index.ts
@@ -6,6 +6,8 @@ import {
   SageSchema,
   SageDocument,
   SageContext,
+  SageCompressedDocumentFormat,
+  SageQuery,
 } from "./type";
 
 import { DocumentContract, QueryContract, SchemaContract } from "./validation";
@@ -14,10 +16,14 @@ import { SageExecutor } from "./execution";
 
 export * from "./assertNotBrowser";
 export * from "./SageError";
+export * from "./problem";
 export * from "./utils";
+
+import { SageProblem } from "./problem";
 
 export const Sage = {
   execute(schema: SageSchema, document: SageDocument, context?: object) {
+  execute(schema: SageSchema, document: SageDocument, context?: SageContext) {
     try {
       // ? if string, parse the request string and get document.
       // ? validate the document
@@ -28,12 +34,36 @@ export const Sage = {
 
       // ? if document is invalid, return an empty execution result with validation errors
 
+      // ? if document is invalid, return an empty execution result with validation problems
       // ? document is valid, so return the execution result
       return SageExecutor.execute(schema, document);
     } catch (error) {}
+    } catch (problem) {}
+  },
+
+  validateSchema(schema: SageSchema): Array<SageProblem> | true {
+    let problems: Array<SageProblem> = [];
+    problems = SchemaContract.validate(document);
+
+    if (problems.length > 0) return problems;
+    else return true;
   },
 
   validateSchema(): boolean {},
+  parse(source: SageCompressedDocumentFormat): {
+    document: SageDocument;
+    problems: Array<SageProblem>;
+  } {
+    let problems: Array<SageProblem> = [];
+    /**
+     *? iterate all properties and do rename:
+     **   typ -> type
+     **   atr -> attributes
+     **   act -> act
+     **   arg -> arguments
+     **   lnk -> links
+     */
+    let document: SageDocument = {};
 
   parse(source: any): SageDocument {},
 
