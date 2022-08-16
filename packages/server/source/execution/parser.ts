dbfe000cc13300cc477f428483a9840edb30bdb1
doruk eray <doruk@dorkodu.com>
2022-08-16 13:20:30 +0300

    implement decompressing and link referancing for sage parser
    - SageParser.decompressQuery()
    - SageParser.replaceLinkReferencesWithQueries()

--- packages/server/source/execution/parser.ts
+++ packages/server/source/execution/parser.ts
@@ -21,6 +21,56 @@ export const SageParser = {
      **   lnk -> links
      */
     let document: SageDocument = {};
+
+    //? iterate all queries
+    for (let [name, compressedQuery] of Object.entries(source)) {
+      // TODO: $query should be SageQuery, but it has readonly properties -> necessary but creates error here
+      let query = this.decompressQuery(compressedQuery);
+      document[name] = query; // place back each $query with extracted/renamed keys to $document
+    }
+
+    document = this.replaceLinkReferencesWithQueries(document);
+
     return { document, problems };
+  },
+
+  decompressQuery(compressedQuery: SageCompressedQuery): SageQuery {
+    let query: any = {};
+
+    //? iterate all query keys and rename
+    for (const [key, value] of Object.entries(compressedQuery)) {
+      switch (key) {
+        case "typ":
+          query.type = value;
+          break;
+        case "atr":
+          query.attributes = value;
+          break;
+        case "act":
+          query.act = value;
+          break;
+        case "arg":
+          query.arguments = value;
+          break;
+        case "lnk":
+          query.links = value;
+          break;
+      }
+    }
+
+    return query;
+  },
+
+  replaceLinkReferencesWithQueries(document: SageDocument): SageDocument {
+    for (let [queryName, query] of Object.entries(document)) {
+      //? replace link name with a reference of that link query, if "links" exists
+      if (query.links) {
+        for (const [linkName, linkReference] of Object.entries(query.links)) {
+          document[queryName].links[linkName] = document[linkReference];
+        }
+      }
+    }
+
+    return document;
   },
 };
