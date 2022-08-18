b26523f258625d621a727c24e0c4bb18cdb9bf63
doruk eray <doruk@dorkodu.com>
2022-08-18 22:34:59 +0300

    restructure tests

--- packages/server/test/schema/index.ts
+++
@@ -1,9 +0,0 @@
-import { Sage } from "../../source";
-
-import Person from "./Person";
-import Artist from "./Artist";
-import Artwork from "./Artwork";
-
-export const schema = Sage.Schema({
-  types: { Person, Artist, Artwork },
-});
