4084d0482d5eb3e92012eff6977cfe3c5cdee9e7
doruk eray <doruk@dorkodu.com>
2022-08-20 02:54:42 +0300

    implement Premise assertion helper and error codes

--- packages/server/source/problem.ts
+++ packages/server/source/problem.ts
@@ -21,13 +21,38 @@ export class SageProblem extends Error {
 
     this.code = code;
     this.cause = cause;
-    this.name = "SageError";
     this.name = 'Sage::Problem';
 
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 
+export function Premise(
+  test: any,
+  message: string = "",
+  cause?: Error
+) {
+    
+  //? create sage problem
+  //* (just that same old error, but rebranded!)
+  let problem = new SageProblem({
+    message: `Sage: [${type.toString()}] ` + message + 
+    (cause
+      ? ` \n[Reason] ${cause.name + cause.message + " \n" + cause?.stack}`
+      : ""),
+    code: 'INTERNAL_SERVER_ERROR',
+    cause
+  });
+
+  // log message example below:
+  // Sage::ERROR: Resource 'User' does not define act 'name'.
+  //? log problem to console
+  //! report only if test fails
+  if (!test) console.error(problem.message);
+  
+  //? return the problem for future use
+  return problem;
+}
 
 export function getMessageFromUnkownError(
   error: unknown,
@@ -60,3 +85,45 @@ export function getErrorFromUnknown(cause: unknown): SageProblem {
   
   return problem;
 }
+
+/**
+ * JSON-RPC 2.0 Error codes
+ * Reference: https://www.jsonrpc.org/specification
+ * `-32000` to `-32099` are reserved for implementation-defined server-errors.
+ * For Sage we're copying the last digits of HTTP 4XX errors.
+ */
+export const SageErrorCodesByKey = {
+  /**
+   * Invalid JSON was received by the server.
+   * An error occurred on the server while parsing the JSON text.
+   */
+  PARSE_ERROR: -32700,
+  
+  /**
+   * The JSON sent is not a valid Sage.Document object.
+   */
+  BAD_REQUEST: -32600, // 400
+
+  /**
+   * Internal error type for user-safe prompting.
+   */
+  INTERNAL_SERVER_ERROR: -32603,
+  
+  //? Implementation specific errors
+  
+  UNAUTHORIZED: -32001,           // 401
+  FORBIDDEN: -32003,              // 403
+  NOT_FOUND: -32004,              // 404
+  METHOD_NOT_SUPPORTED: -32005,   // 405
+  TIMEOUT: -32008,                // 408
+  CONFLICT: -32009,               // 409
+  PRECONDITION_FAILED: -32012,    // 412
+  PAYLOAD_TOO_LARGE: -32013,      // 413
+  CLIENT_CLOSED_REQUEST: -32099,  // 499
+} as const;
+
+export const SageErrorCodesByNumber = invert(SageErrorCodesByKey);
+type ValueOf<T> = T[keyof T];
+
+export type SageErrorCodeNumber = ValueOf<typeof SageErrorCodesByKey>;
+export type SageErrorCodeKey = keyof typeof SageErrorCodesByKey;
