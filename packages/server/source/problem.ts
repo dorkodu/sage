1eeae0e4d9d34c0813e23f8af29223c825e2f3d1
doruk eray <doruk@dorkodu.com>
2022-08-20 02:54:06 +0300

    implementing namespace: problem
    update SageProblem class and add error infering functions:
    getErrorFromUnknown
    getMessageFromUnknownError

--- packages/server/source/problem.ts
+++ packages/server/source/problem.ts
@@ -1,8 +1,4 @@
-const getMessageFromProblemCause = (cause: Error): string => {
-  let message: string = "";
-
-  return message;
-};
+import { invert } from './utils';
 
 export class SageProblem extends Error {
   public readonly cause?;
@@ -10,13 +6,14 @@ export class SageProblem extends Error {
 
   constructor(opts: {
     message?: string;
-    code: number;
+    code: SageErrorCodeKey;
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore This will be `Error` in next major version
     cause?: Error;
   }) {
     const cause = opts.cause;
     const code = opts.code;
-    const message = opts.message ?? getMessageFromProblemCause();
+    const message = opts.message ?? getMessageFromUnkownError(cause, code);
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore https://github.com/tc39/proposal-error-cause
@@ -25,11 +22,41 @@ export class SageProblem extends Error {
     this.code = code;
     this.cause = cause;
     this.name = "SageError";
+    this.name = 'Sage::Problem';
 
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 
-enum SageErrorCode {
-  1,
+
+export function getMessageFromUnkownError(
+  error: unknown,
+  fallback: string,
+): string {
+  
+  if (typeof error === 'string') 
+    return error;
+  if (error instanceof Error && typeof error.message === 'string')
+    return error.message;
+
+  return fallback;
+}
+
+export function getErrorFromUnknown(cause: unknown): SageProblem {
+  // this should ideally be an `instanceof SageProblem` but tRPC source code says that isn't working for some reason :/
+  // ref https://github.com/trpc/trpc/issues/331
+  if (cause instanceof SageProblem && cause.name === 'Sage::Problem')
+    return cause as SageProblem;
+
+  const problem = new SageProblem({
+    code: 'INTERNAL_SERVER_ERROR',
+    cause,
+  });
+
+  // take stack trace from cause
+  if (cause instanceof Error) {
+    problem.stack = cause.stack;
+  }
+  
+  return problem;
 }
