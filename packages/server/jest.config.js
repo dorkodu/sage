8d8b43e76ed1714bee469226455ef0ce8dfe8080
doruk eray <doruk@dorkodu.com>
2022-08-19 23:57:59 +0300

    update package configs

---
+++ packages/server/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  rootDir: '../',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  collectCoverageFrom: ['./*/src/**/*.{ts,tsx,js,jsx}'],
+  // setupFilesAfterEnv: ['./server/jest.setup.js'],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        jsx: 'react',
+        target: 'ES2020',
+      },
+    },
+  },
+  // setupFiles: ['core-js', 'regenerator-runtime/runtime'],
+};
