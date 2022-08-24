8919bb0a2457d75c63b10a5f59b29449c41a19bd
doruk eray <doruk@dorkodu.com>
2022-08-25 02:00:24 +0300

    create a reference procedure: Sage.run()

--- packages/server/source/sage.ts
+++ packages/server/source/sage.ts
@@ -19,6 +19,62 @@ import { SageExecutor } from "./execution";
 import { ProcedureResult } from "./utils";
 
 export const Sage = {
+  run(
+    source: SageSimplifiedDocument,
+    schema: SageSchema,
+    context: SageContext = {},
+  ): SageResponse {
+    // empty result and response
+    let result: SageExecutionResult = SageExecutor.emptyExecutionResult();
+    let response: SageResponse = SageParser.response(result);
+
+    //? parse document
+    const parsed = SageParser.parse(source);
+    const document = parsed.document;
+
+    //? validate document
+    const documentValidationResult = this.validate(DocumentContract, document);
+
+    if (!documentValidationResult.data) {
+      let documentErrors: SageErrorOutput[] =
+        documentValidationResult.errors.map((e) => {
+          return SageParser.outputError(e);
+        });
+
+      response.errors = {
+        $document: documentErrors,
+      };
+
+      return response;
+    }
+
+    //? validate schema ONLY IF not verified
+    if (!schema.verified) {
+      const schemaValidationResult = this.validate(SchemaContract, schema);
+
+      if (!schemaValidationResult.data) {
+        let schemaErrors: SageErrorOutput[] = schemaValidationResult.errors.map(
+          (e) => {
+            return SageParser.outputError(e);
+          },
+        );
+
+        response.errors = {
+          $schema: schemaErrors,
+        };
+
+        return response;
+      }
+    }
+
+    //? execute
+    //! ...ONLY IF both document and schema are valid
+    const executionResult = SageExecutor.execute(schema, document, context);
+    response = SageParser.response(executionResult);
+
+    return response;
+  },
+
   execute(
     document: SageDocument,
     schema: SageSchema,
