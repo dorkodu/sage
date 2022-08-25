55f3171e6b09bef956a3118b09eeb7ccbf239def
doruk eray <doruk@dorkodu.com>
2022-08-25 22:58:24 +0300

    add new type definitions to client

--- packages/client/source/type.ts
+++ packages/client/source/type.ts
@@ -1,1 +1,12 @@
 export * from "../../server/source/type";
+import { SageQuery, SageResponse } from "../../server/source/type";
+
+export interface SageDataRequirement {
+  query: SageQuery;
+  fetch: () => Promise<SageResponse>;
+}
+
+export interface SageDataSource {
+  url: string;
+  headers?: { [key: string]: string };
+}
