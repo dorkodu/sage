1c10493064e08429cc6d4fa9e2c8a571f91cdbde
doruk eray <doruk@dorkodu.com>
2022-08-28 13:39:13 +0300

    [examples] implement a http data source

---
+++ examples/http.ts
@@ -0,0 +1,37 @@
+import {
+  SageDataSource,
+  SageDocument,
+  SageResponse,
+  SageSimplifiedDocument,
+} from "@dorkodu/sage.client";
+
+export class SageHTTPSource implements SageDataSource {
+  url: string;
+  headers: { [key: string]: string } = {};
+
+  constructor(opts: { url: string; headers?: { [key: string]: string } }) {
+    this.url = opts.url;
+    this.headers = opts.headers ?? {};
+  }
+
+  async retrieve(document: SageDocument) {
+    const simplifiedDocument: SageSimplifiedDocument = {};
+
+    const request: RequestInit = {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify(simplifiedDocument),
+    };
+
+    const response = await fetch(this.url, request);
+    const json = await response.json();
+
+    const result: SageResponse = {
+      data: json.data,
+      errors: json.errors,
+      meta: json.meta,
+    };
+
+    return result;
+  }
+}
