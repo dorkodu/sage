ba3a2f08ea02d3a0a21a8a45fbe091afe2e9e666
doruk eray <doruk@dorkodu.com>
2022-08-28 13:39:39 +0300

    [examples] update files: client, server

--- examples/client.ts
+++ examples/client.ts
@@ -1,3 +1,28 @@
-import { Sage as SageClient } from "./packages/client/source";
+import { Sage, SageQuery } from "@dorkodu/sage.client";
+import { SageHTTPSource } from "./http";
 
-const user = Sage.want();
+import { Schema as DataSchema } from "./server";
+
+const API = new SageHTTPSource({
+  url: "http://localhost:2004",
+  headers: {},
+});
+
+const sage = new Sage<DataSchema>({ source: API });
+
+const me = sage.want({
+  resource: "Me",
+  attributes: ["name", "email", "token"],
+  act: "refresh",
+  arguments: {
+    id: 2152,
+  },
+});
+
+const result = me.fetch();
+
+const query: SageQuery = {
+  resource:
+}
+
+sage.request({ })
--- examples/server.ts
+++ examples/server.ts
@@ -4,11 +4,13 @@ import {
   SageResponse,
 } from "../packages/server/source";
 import { schema } from "./schema";
-import { request } from "./client";
 
 let context = {
   me: 42, // logged in user id
 };
 
 //? SHORTCUT
-const response: SageResponse = Sage.run(request, schema, context);
+const response: SageResponse = Sage.run({}, schema, context);
+const json = JSON.stringify(response);
+
+export type Schema = typeof schema;
